b6b73997fc5ccf80bb0a66f30096c31fa357574c
Kyle Evans <kevans91@ksu.edu>
2014-12-05 15:09:52 -0500

    Modifier without an explicit type is an implied int

--- src/m_menu.cpp
+++ src/m_menu.cpp
@@ -900,7 +900,7 @@ void M_VerifyNightmare(int ch)
     if (ch != 'y')
 	return;
 		
-    G_DeferedInitNew(nightmare,epi+1,1);
+    G_DeferedInitNew((skill_t)nightmare,epi+1,1);
     M_ClearMenus ();
 }
 
